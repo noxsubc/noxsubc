206fa90fbb44d80a88bba81f22ad4ac8f1ffa095
Philippe <noxsubph13@gmail.com>
2025-07-25 00:08:21 -0300

    Primeiro commit

---
+++ app.py
@@ -0,0 +1,16 @@
+﻿from flask import Flask, send_from_directory
+from flask_cors import CORS
+
+app = Flask(__name__, static_folder='static')
+CORS(app)
+
+@app.route('/')
+def serve_frontend():
+    return send_from_directory(app.static_folder, 'index.html')
+
+@app.route('/api/message')
+def get_message():
+    return {"message": "Hello from Python Backend!"}
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10000)
