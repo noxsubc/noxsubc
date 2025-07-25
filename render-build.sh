9708be447ea3e471ca613ff556871f568f6a65bc
Philippe <noxsubph13@gmail.com>
2025-07-25 00:54:18 -0300

    Teste4

---
+++ render-build.sh
@@ -0,0 +1,4 @@
+﻿#!/usr/bin/env bash
+apt-get update && \
+apt-get install -y ffmpeg libsm6 libxext6 libavformat-dev libavcodec-dev libavdevice-dev libavutil-dev libavfilter-dev libswscale-dev libswresample-dev
+pip install -r requirements.txt
